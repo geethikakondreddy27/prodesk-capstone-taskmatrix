0679f3d6a1eb1a43214d5f942b8cd565c7a753e2
Geethika Kondreddy <geethikakondreddy@gmail.com>
2026-07-02 18:02:24 +0530

    Complete React authentication and JWT client integration

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,69 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../services/authService";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await loginUser(formData);
+
+      localStorage.setItem("token", data.token);
+
+      alert("Login Successful!");
+
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error);
+
+      alert("Invalid Credentials");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
